cac5a2a5d8c730c2d37e9c8afffc57a4dfe8020a
Marcos <marcoscrac36@gmail.comnode_modules>
2026-05-12 19:34:23 -0300

    RESET dynamic vehicle page clean

--- src/app/vehiculo/[id]/page.tsx
+++ src/app/vehiculo/[id]/page.tsx
@@ -5,6 +5,7 @@ import { vehicles } from "@/db/schema";
 import { notFound } from "next/navigation";
 
 export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 
 export default async function VehiclePage({
   params,
